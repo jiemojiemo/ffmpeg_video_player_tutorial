900430c2fd88f3850d51d293b2fe28e530e91743
william <460706836@qq.com>
2023-11-09 21:24:30 +0800

    feat: add ffmpeg av decoder

--- tests/modules/test_ffmpeg_audio_decoder.cpp
+++ tests/modules/test_ffmpeg_audio_decoder.cpp
@@ -1,48 +1,48 @@
 //
-// Created by user on 11/6/23.
+// Created by user on 11/9/23.
 //
 
-#include "j_video_player/modules/j_ffmpeg_video_decoder.h"
+#include "j_video_player/modules/j_ffmpeg_av_decoder.h"
 #include <gmock/gmock.h>
 
 using namespace testing;
 using namespace j_video_player;
 
-class AFFmpegVideoDecoder : public Test {
+class AFFmpegAudioDecoder : public Test {
 public:
   void SetUp() override {}
 
-  std::unique_ptr<FFmpegVideoDecoder> decoder =
-      std::make_unique<FFmpegVideoDecoder>();
+  std::unique_ptr<IAudioDecoder> decoder =
+      std::make_unique<FFmpegAudioDecoder>();
   std::string file_path =
       "/Users/user/Documents/work/测试视频/video_1280x720_30fps_30sec.mp4";
 };
 
-TEST_F(AFFmpegVideoDecoder, IsInvalidBeforeOpen) {
+TEST_F(AFFmpegAudioDecoder, IsInvalidBeforeOpen) {
   ASSERT_THAT(decoder->isValid(), Eq(false));
 }
 
-TEST_F(AFFmpegVideoDecoder, OpenFileSuccessReturns0) {
+TEST_F(AFFmpegAudioDecoder, OpenFileSuccessReturns0) {
   auto ret = decoder->open(file_path);
 
   ASSERT_THAT(ret, Eq(0));
 }
 
-TEST_F(AFFmpegVideoDecoder, IsValidAfterOpenSuccess) {
+TEST_F(AFFmpegAudioDecoder, IsValidAfterOpenSuccess) {
   auto ret = decoder->open(file_path);
 
   ASSERT_THAT(ret, Eq(0));
   ASSERT_THAT(decoder->isValid(), Eq(true));
 }
 
-TEST_F(AFFmpegVideoDecoder, IsValidAfterOpenFailed) {
+TEST_F(AFFmpegAudioDecoder, IsValidAfterOpenFailed) {
   auto ret = decoder->open("invalid_file_path");
 
   ASSERT_THAT(ret, Ne(0));
   ASSERT_THAT(decoder->isValid(), Eq(false));
 }
 
-TEST_F(AFFmpegVideoDecoder, IsInvalidAfterClose) {
+TEST_F(AFFmpegAudioDecoder, IsInvalidAfterClose) {
   auto ret = decoder->open(file_path);
   ASSERT_THAT(ret, Eq(0));
   ASSERT_THAT(decoder->isValid(), Eq(true));
@@ -51,64 +51,64 @@ TEST_F(AFFmpegVideoDecoder, IsInvalidAfterClose) {
   ASSERT_THAT(decoder->isValid(), Eq(false));
 }
 
-TEST_F(AFFmpegVideoDecoder, DecodeReturnsNullIfInvalid) {
+TEST_F(AFFmpegAudioDecoder, DecodeReturnsNullIfInvalid) {
   ASSERT_THAT(decoder->isValid(), Eq(false));
 
-  auto frame = decoder->decodeNextFrame();
+  auto frame = decoder->decodeNextAudioFrame();
   ASSERT_THAT(frame, IsNull());
 }
 
-TEST_F(AFFmpegVideoDecoder, CanDecodeNextVideoFrame) {
+TEST_F(AFFmpegAudioDecoder, CanDecodeNextAudioFrame) {
   auto ret = decoder->open(file_path);
   ASSERT_THAT(ret, Eq(0));
 
-  auto frame = decoder->decodeNextFrame();
+  auto frame = decoder->decodeNextAudioFrame();
   ASSERT_THAT(frame, NotNull());
 }
 
-TEST_F(AFFmpegVideoDecoder, PositionIsNOValueBeforeDecode) {
+TEST_F(AFFmpegAudioDecoder, PositionIsNOValueBeforeDecode) {
   auto ret = decoder->open(file_path);
   ASSERT_THAT(ret, Eq(0));
 
   ASSERT_THAT(decoder->getPosition(), Eq(AV_NOPTS_VALUE));
 }
 
-TEST_F(AFFmpegVideoDecoder, PositionSameWithFramePTS) {
+TEST_F(AFFmpegAudioDecoder, PositionSameWithFramePTS) {
   decoder->open(file_path);
 
   for (int i = 0; i < 3; i++) {
-    auto frame = decoder->decodeNextFrame();
-    auto time_base = decoder->getVideoStreamTimeBase();
+    auto frame = decoder->decodeNextAudioFrame();
+    auto time_base = decoder->getMediaFileInfo().audio_stream_timebase;
     auto expected_pts = av_rescale_q(frame->f->pts, time_base, AV_TIME_BASE_Q);
-    ASSERT_THAT(decoder->getPosition(), Eq(expected_pts));
+    EXPECT_THAT(decoder->getPosition(), Eq(expected_pts));
   }
 }
 
-TEST_F(AFFmpegVideoDecoder, SeekVideoFrameQuickCanGetVideoFrame) {
+TEST_F(AFFmpegAudioDecoder, SeekAudioFrameQuickCanGetAudioFrame) {
   auto ret = decoder->open(file_path);
   ASSERT_THAT(ret, Eq(0));
 
   auto target_pts = 1 * 1000 * 1000; // 1s
-  auto frame = decoder->seekVideoFrameQuick(target_pts);
+  auto frame = decoder->seekAudioFrameQuick(target_pts);
   ASSERT_THAT(frame, NotNull());
 }
 
-TEST_F(AFFmpegVideoDecoder, SeeVideoFrameQuickFailedIfInvalid) {
+TEST_F(AFFmpegAudioDecoder, SeeAudioFrameQuickFailedIfInvalid) {
   ASSERT_THAT(decoder->isValid(), Eq(false));
-  ASSERT_THAT(decoder->seekVideoFrameQuick(0), IsNull());
+  ASSERT_THAT(decoder->seekAudioFrameQuick(0), IsNull());
 }
 
-TEST_F(AFFmpegVideoDecoder, SeekFramePreciseCanGetVideoFrame) {
+TEST_F(AFFmpegAudioDecoder, SeekFramePreciseCanGetAudioFrame) {
   auto ret = decoder->open(file_path);
   ASSERT_THAT(ret, Eq(0));
 
   auto target_pts = 1 * 1000 * 1000; // 1s
-  auto frame = decoder->seekVideoFramePrecise(target_pts);
+  auto frame = decoder->seekAudioFramePrecise(target_pts);
   ASSERT_THAT(frame, NotNull());
   ASSERT_THAT(frame->f->pts, Not(0));
 }
 
-TEST_F(AFFmpegVideoDecoder, InvalidAfterClose) {
+TEST_F(AFFmpegAudioDecoder, InvalidAfterClose) {
   auto ret = decoder->open(file_path);
   ASSERT_THAT(ret, Eq(0));
 
@@ -116,28 +116,24 @@ TEST_F(AFFmpegVideoDecoder, InvalidAfterClose) {
   ASSERT_THAT(decoder->isValid(), Eq(false));
 }
 
-TEST_F(AFFmpegVideoDecoder, PositionResetToNOPTSAfterClose) {
+TEST_F(AFFmpegAudioDecoder, PositionResetToNOPTSAfterClose) {
   auto ret = decoder->open(file_path);
   ASSERT_THAT(ret, Eq(0));
 
-  decoder->decodeNextFrame();
+  decoder->decodeNextAudioFrame();
   ASSERT_THAT(decoder->getPosition(), Ne(AV_NOPTS_VALUE));
 
   decoder->close();
   ASSERT_THAT(decoder->getPosition(), Eq(AV_NOPTS_VALUE));
 }
 
-TEST_F(AFFmpegVideoDecoder, GetVideoFileInfoAsExpected) {
+TEST_F(AFFmpegAudioDecoder, GetMediaFileInfoAsExpected) {
   auto ret = decoder->open(file_path);
   ASSERT_THAT(ret, Eq(0));
 
-  auto info = decoder->getVideoFileInfo();
+  auto info = decoder->getMediaFileInfo();
   EXPECT_THAT(info.file_path, Eq(file_path));
-  EXPECT_THAT(info.width, Eq(1280));
-  EXPECT_THAT(info.height, Eq(720));
-  EXPECT_THAT(info.duration, Eq(30000000));
-  EXPECT_THAT(info.bit_rate, Eq(304871));
-  EXPECT_THAT(info.fps, Eq(30));
-  EXPECT_THAT(info.video_stream_timebase.num, Eq(1));
-  EXPECT_THAT(info.video_stream_timebase.den, Eq(15360));
+  EXPECT_THAT(info.sample_format, Eq(AV_SAMPLE_FMT_FLTP));
+  EXPECT_THAT(info.sample_rate, Eq(44100));
+  EXPECT_THAT(info.channels, Eq(2));
 }
