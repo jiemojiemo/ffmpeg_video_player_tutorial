e6b43887181c11a8eddc58573baf82ea310f96df
william <460706836@qq.com>
2023-11-12 21:35:13 +0800

    rename

--- include/j_video_player/modules/j_i_source.h
+++ include/j_video_player/modules/j_i_source.h
@@ -1,21 +1,35 @@
 //
-// Created by user on 5/20/23.
+// Created by user on 11/11/23.
 //
 
 #ifndef FFMPEG_VIDEO_PLAYER_J_I_SOURCE_H
 #define FFMPEG_VIDEO_PLAYER_J_I_SOURCE_H
+#include "j_video_player/modules/j_frame.h"
+#include "j_video_player/modules/j_media_file_info.h"
 
 namespace j_video_player {
+enum class SourceState {
+  kIdle,
+  kStopped,
+  kPlaying,
+  kSeeking,
+  kPaused,
+};
 class ISource {
 public:
   virtual ~ISource() = default;
-  virtual void start() = 0;
-  virtual void pause() = 0;
-  virtual void stop() = 0;
-  virtual void seek(float pos) = 0;
-  virtual float getDuration() = 0;
-  virtual float getCurrentPosition() = 0;
-  virtual bool isPlaying() const = 0;
+
+  virtual int open(const std::string &file_path) = 0;
+  virtual MediaFileInfo getMediaFileInfo() = 0;
+  virtual int play() = 0;
+  virtual int pause() = 0;
+  virtual int stop() = 0;
+  virtual int seek(int64_t timestamp) = 0;
+  virtual SourceState getState() = 0;
+  virtual int64_t getDuration() = 0;
+  virtual int64_t getCurrentPosition() = 0;
+  virtual std::shared_ptr<Frame> dequeueFrame() = 0;
+  virtual int getQueueSize() = 0;
 };
 } // namespace j_video_player
 
