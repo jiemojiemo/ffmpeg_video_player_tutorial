f110a04df487cee2778351b2642a06685f5f833c
william <460706836@qq.com>
2023-11-17 11:21:47 +0800

    add DisplayVideoActivity.kt

--- android/tutorial02_surfaceview/app/src/main/java/com/example/usesurfaceview/DisplayVideoActivity.kt
+++ android/tutorial02_surfaceview/app/src/main/java/com/example/usesurfaceview/DisplayVideoActivity.kt
@@ -4,8 +4,12 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
 class DisplayVideoActivity : AppCompatActivity() {
+    private lateinit var mSurfaceView: MySurfaceView
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_display_video)
+
+        mSurfaceView = findViewById(R.id.surfaceView_display_video)
+
     }
 }
